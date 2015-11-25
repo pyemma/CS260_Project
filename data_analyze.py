9cd76d55c8ed65ecb593b76632c7d615dba2277e
pyemma <pyemma1991@gmail.com>
2015-11-25 00:56:02 -0800

    tried logistic regression

--- data_analyze.py
+++ data_analyze.py
@@ -15,6 +15,12 @@ import preprocess
 #     print("Common values in " + fields[index] + ": " + str(count))
 #     index = index + 1
 
-train_data = preprocess.group_by_visit_number('data/train.csv')
+train_label, raw_train_data = preprocess.group_by_visit_number('data/train.csv')
+test_label, raw_test_data = preprocess.group_by_visit_number('data/test.csv', False)
 
-print(len(train_data))
+processed_train_data = preprocess.process_data(raw_train_data, ['s', 'd'])
+processed_test_data = preprocess.process_data(raw_test_data, ['s', 'd'])
+
+bag_of_features = preprocess.get_feature_bag(processed_train_data, processed_test_data, {})
+
+train_data = preprocess.one_hot_encoding(processed_train_data, bag_of_features)
--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,12 @@
 import csv
 import collections
+import numpy as np
+import scipy.sparse
+from scipy.sparse import csr_matrix
 """process train data and test data"""
 
 
-def process_data(filename=None, training=True):
+def analyze_data(filename=None, training=True):
     if filename is None:
         raise ValueError("filename should not be None")
 
@@ -50,7 +53,7 @@ def process_data(filename=None, training=True):
 
 def group_by_visit_number(filename=None, train=True):
     """ data would be transformed to format like
-     (trip_type, (visit_number, weekday), [(upc, count, desc, fine_line)]) """
+        (trip_type, (visit_number, weekday, [(upc, count, desc, fine_line)])) """
 
     if filename is None:
         raise ValueError("Filename should not be none")
@@ -73,4 +76,108 @@ def group_by_visit_number(filename=None, train=True):
             else:
                 aggregrated_data[data[1]] = data
 
-    return aggregrated_data
+    labels = []
+    raw_data = []
+    for (key, value) in aggregrated_data.items():
+        label = value[0]
+        data = (value[1][0], value[1][1], value[2])
+        labels.append(label)
+        raw_data.append(data)
+
+    return (labels, raw_data)
+
+
+def get_filter(fields_to_use=['u', 's', 'd', 'f'], reserve_visit_number=False):
+    """ a helper function to generate a filter which will
+        filter the raw data with the selected fiedls, this
+        would only apply to the third term in the data representation
+        return a lambda function which could apply to the raw data
+        u: upc
+        s: scan_count
+        d: department_description
+        f: fine_line_number """
+    raw_fields = ['u', 's', 'd', 'f']
+    mask_array = []
+    for elem in raw_fields:
+        if elem in fields_to_use:
+            mask_array.append(True)
+        else:
+            mask_array.append(False)
+
+    data_filter = None
+    mask_array = np.array(mask_array)
+    if reserve_visit_number is True:
+        data_filter = lambda (x0, x1, x2): (x0, x1, map(lambda x: tuple(np.array(x)[mask_array]), x2))
+    else:
+        data_filter = lambda (x0, x1, x2): (x1, map(lambda x: tuple(np.array(x)[mask_array]), x2))
+
+    return data_filter
+
+
+def process_data(train_data=None,
+                 fields_to_use=['u', 's', 'd', 'f'],
+                 reserve_visit_number=False):
+    """ filter data according to the fields to use """
+    data_filter = get_filter(fields_to_use, reserve_visit_number)
+
+    if train_data is None:
+        raise ValueError("Train data should not be none")
+
+    return map(data_filter, train_data)
+
+
+def __update_bag(data=None, bag={}):
+
+    dim = len(data[0])
+
+    for row in data:
+        for i in range(0, dim):
+            if type(row[i]) is not list:
+                if row[i] not in bag:
+                    bag[row[i]] = len(bag)
+            else:
+                for elem in row[i]:
+                    if elem not in bag:
+                        bag[elem] = len(bag)
+
+    return bag
+
+
+def one_hot_encoding(data=None, bag={}):
+    total_feature = len(bag)
+    num_data = len(data)
+    dim = len(data[0])
+    sparse_data = np.zeros([num_data, total_feature])
+    count = 0
+
+    for j in range(0, num_data):
+        row = data[j]
+        for i in range(0, dim):
+            if type(row[i]) is not list:
+                index = bag[row[i]]
+                sparse_data[j][index] = 1
+            else:
+                for elem in row[i]:
+                    index = bag[elem]
+                    sparse_data[j][index] = 1
+        count = count + 1
+        if count % 1000 == 0:
+            print("Processed " + str(count))
+
+    return sparse_data
+
+
+def get_feature_bag(train_data=None,
+                    test_data=None,
+                    bag_of_features={}):
+    """ one hot encoding of the data,
+        retunr sparse matrix """
+
+    assert(len(train_data[0]) == len(test_data[0]))
+
+    bag_of_features = __update_bag(train_data, bag_of_features)
+    bag_of_features = __update_bag(test_data, bag_of_features)
+    total_feature = len(bag_of_features)
+    print("Total feature number in bag: " + str(total_feature))
+
+    return bag_of_features
